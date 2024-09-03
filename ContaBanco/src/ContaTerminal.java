de8284fb87623980a7c63d707e92ef0a2eb45c6e
Jefferson Souza Jr <96705369+Jrsouza8315@users.noreply.github.com>
2024-09-02 23:29:42 -0300

    mensagens

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //todo: Conhecer e importar a classe scanner
+        //      Exibir as mensagens para nosso usuário
+        //      Obter pela scanner os valores digitados no terminal 
+        //      Exibir a mensagem conta criada
     }
 }
